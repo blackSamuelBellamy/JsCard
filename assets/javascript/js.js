13d296160b516c4a2d350fcef00628f9ee06190f
Alejandro <alejandro.quinones@outlook.es>
2022-06-07 22:53:35 -0400

    style4

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,8 +1,18 @@
+const card = document.querySelector('.card');
 const next = document.querySelector('.next');
 const previous = document.querySelector('.previous');
 const number = document.querySelector('.number');
 const price = document.querySelector('.price');
 const button = document.querySelector('.boton');
+
+const colors = document.querySelector('.colors');
+const rojo = document.querySelector('.rojo');
+const azul = document.querySelector('.azul');
+const negro = document.querySelector('.negro');
+const amarillo = document.querySelector('.amarillo');
+const verde = document.querySelector('.verde');
+const ul = document.querySelector('ul');
+let color;
 
 next.addEventListener('click', () => {
     number.textContent ++;
@@ -20,6 +30,26 @@ previous.addEventListener('click', () => {
     }
    
 });
+
+
+
+colors.addEventListener('click', () => {
+
+    ul.style.display = 'block';
+    card.style.opacity = '.1';
+    button.style.display ='none';
+
+})
+
+ul.addEventListener('click', () =>{
+
+    ul.style.display = 'none';
+    card.style.opacity = '1';
+    button.style.display = 'block'; 
+
+})
+
+  
 
 
 
